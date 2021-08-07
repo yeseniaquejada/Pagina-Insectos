60a9c95468710596f15e48e292941c6c8e4e220f
AXG25 <axiever.guejia@gmail.com>
2021-08-06 19:07:12 -0500

    js

---
+++ Script/script.js
@@ -0,0 +1,11 @@
+const screens = document.querySelectorAll('.screen')
+
+const choose_insect_btns = document.querySelectorAll('.choose_insect_btns')
+const start_btn = document.querySelectorAll('.start_btn')
+const game_container = document.querySelectorAll('.game_container')
+
+const tiemEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const messageEl = document.getElementById('message')
+
+let seconds
