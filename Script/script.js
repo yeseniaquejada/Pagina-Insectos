63a598b807e0c866467df2856b07a6a7429f40fe
Tony-404 <chernani2003@gmail.com>
2021-08-06 19:37:18 -0500

    actualizacion js

--- Script/script.js
+++ Script/script.js
@@ -43,4 +43,26 @@ function createInsect() {
     const insect = document.createElement('div')
     insect.classList.add('insect')
     const {x,y} = getRandomLocation()
+    insect.style.top=`${y}px`
+    insect.style.left=`${x}px`
+    insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}" style="transform: rotate(${Math.random()*360}deg)" />` 
+    insect.addEventListener('click', catchInsect)
+    game_container.appendchild(insect)
+}
+
+function getRandomLocation() {
+    const width = window.innerWidht
+    const height = window.innerHeight
+    const x = Math.random()*(width -200) + 100
+    const y = Math.random() * (height -200) + 1000
+
+    return{x,y}
+}
+
+
+function catchInsect() {
+    increaseScore()
+    this.classList.add('caught')
+    setTimeout(() => this.renove(), 2000)
+    addInsects()
 }
